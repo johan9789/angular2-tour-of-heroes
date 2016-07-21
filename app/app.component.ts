48ad885a93944760c91cc79fc8e94c5bdad41434
Johan <jmpv567@gmail.com>
2016-07-21 16:42:09 -0500

    Services (Tutorial - Final)

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import { HeroService } from './hero.service';
 
+import './rxjs-extensions';
+
 @Component({
     selector: 'my-app',
     styleUrls: ['app/app.component.css'],
--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,15 +1,9 @@
-import { Component } from '@angular/core';
-
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroService } from './hero.service';
-import { OnInit } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 
-export class Hero {
-    id: number;
-    name: string;
-}
+import {Hero}  from './hero';
+import {HeroService} from './hero.service';
+import {HeroDetailComponent} from './hero-detail.component';
 
 @Component({
     selector: 'my-app',
@@ -20,9 +14,10 @@ export class Hero {
 })
 
 export class HeroesComponent implements OnInit {
-    title = 'Tour of Heroes';
     heroes: Hero[];
     selectedHero: Hero;
+    addingHero = false;
+    error: any;
 
     constructor(private router: Router, private heroService: HeroService){}
 
@@ -40,6 +35,28 @@ export class HeroesComponent implements OnInit {
 
     gotoDetail() {
         this.router.navigate(['/detail', this.selectedHero.id]);
+    }
+
+    addHero(){
+        this.addingHero = true;
+        this.selectedHero = null;
+    }
+
+    close(savedHero: Hero){
+        this.addingHero = false;
+        if (savedHero){
+            this.getHeroes();
+        }
+    }
+
+    deleteHero(hero: Hero, event: any){
+        event.stopPropagation();
+        this.heroService.delete(hero).then(res => {
+            this.heroes = this.heroes.filter(h => h !== hero);
+            if(this.selectedHero === hero){
+                this.selectedHero = null;
+            }
+        }).catch(error => this.error = error);
     }
 
 }
